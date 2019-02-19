d5b36efc64f7864103c9dbbfaec5ca9924aa6f24
Kenny Armstrong <36676708+kenfrd@users.noreply.github.com>
2019-02-19 17:00:52 -0500

    added hi_there.cpp
    Practice C++ file to compile with.

---
+++ hi_there.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// Just a semple test program that displays text to the screen.
+//
+
+int main()
+{
+	cout << "Hello there!  How are you?" << endl;
+	return 0;
+}
